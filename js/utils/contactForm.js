8ea4a7a2caa3414b6ca0376ffb526755cb83f46d
Alan62 <beaucheron.alan@free.fr>
2022-02-25 14:13:13 +0100

    contactform / launch / close / name in the header contactform

--- js/utils/contactForm.js
+++ js/utils/contactForm.js
@@ -1,9 +1,41 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
+'use strict';
+/////////////////////////////////////////
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+export default class Modal {
+    // Events, launch/close the modal by clicking on the 'contact me' button
+    modal(data) {
+        let modalBtn = document.getElementById("ph-contact");
+        let closeBtn = document.getElementById('close-btn');
+
+        if (modalBtn) {
+            modalBtn.addEventListener('click', this.launchModal);
+            this.formPhName(data);
+        }
+        if (closeBtn) {
+            closeBtn.addEventListener('click', this.closeModal);
+        }
+    }
+
+    // LAUNCH MODAL
+    launchModal() {
+        let modalbg = document.getElementById("form-dialog");
+
+        modalbg.style.display = 'block';
+    }
+
+    // CLOSE MODAL
+    closeModal() {
+        let modalbg = document.getElementById("form-dialog");
+
+        modalbg.style.display = 'none';
+    }
+
+    // DISPLAY PH NAMES IN FORM
+    formPhName(data) {
+        let id = window.location.search.split('id=')[1];
+        let photographers = !id ? data : data.filter(photographer => photographer.id == id);
+        let phName = document.getElementById('contact-form-name');
+        let phNameTemplate = `${photographers[0].name}`
+        phName.innerHTML = phNameTemplate;
+    }
 }
