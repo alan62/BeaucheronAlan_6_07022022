fcfd9ad418b1de451e99f0635a5cc846e7d9afc7
Alan62 <beaucheron.alan@free.fr>
2022-02-25 14:16:40 +0100

    get data from json

---
+++ js/provider/ApiFishEye.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// GET THE DATA from JSON file
+export default class ApiFishEye {
+    async getData() {
+        let url = "/Api/FishEye/photographers.json";
+        let response = await fetch(url);
+        let data = await response.json();
+
+        const dataPhotographers = [...data.photographers];
+        const dataMedias = [...data.media];
+
+        return {
+            'photographers': dataPhotographers,
+            'media': dataMedias
+        };
+    }
+}
