66266be8b7ab9f901787f354c183b21a7020d69b
Alan62 <beaucheron.alan@free.fr>
2022-03-03 17:23:39 +0100

    display data sur la page profil du photographe + clic icone like

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
-'use strict';
-/////////////////////////////////////////
+'use strict'
 
 // DATA
-import ApiFishEye from './provider/ApiFishEye.js';
+import ApiFishEye from './provider/ApiFishEye.js'
 
 // HOMEPAGE
-import HomePageBuilder from './home/HomePageBuilder.js';
+import HomePageBuilder from './home/HomePageBuilder.js'
 
+// PHOTOGRAPHE PAGES
 
-(function appDispatch() {
-    new ApiFishEye().getDataFishEye().then((data) => {
-        if (window.location.pathname.includes("/photographers.html")) {
-            // PHOTOGRAPHER PROFIL HEADER à venir
-        }
-        // affichage des données sur la page d'accueil
-        new HomePageBuilder().displayPhotographers(data); 
-    })
-})();
+import Profil from './photographers/Profil.js'
+import MediaBuilder from './photographers/MediaBuilder.js'
+import DropDownMenu from './photographers/DropDown.js'
+
+(function appDispatch () {
+  new ApiFishEye().getData().then((data) => {
+    if (window.location.pathname.includes('/photographer.html')) {
+      new Profil().displayProfil(data)
+      // DROPDOWNMENU
+      new DropDownMenu().dropDown(data)
+
+      // gallery & likes box
+      new MediaBuilder().photographersMedias(data)
+      return
+    }
+    // show details into homepage
+    new HomePageBuilder().displayPhotographers(data)
+  }).catch(() => {
+    console.error('Failed to load data')
+  })
+})()
